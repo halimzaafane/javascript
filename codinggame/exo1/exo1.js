ba3bc605a50cac16486b7edaa4689277e41af764
halimzaafane <109077943+halimzaafane@users.noreply.github.com>
2022-07-11 16:54:16 +0200

    Update exo1.js

--- codinggame/exo1/exo1.js
+++ codinggame/exo1/exo1.js
@@ -1,1 +1,4 @@
-document.getElementById('titre').textContent = "Rick Astley - Never Gonna Give You Up";
+//document.getElementById('titre').textContent = "Rick Astley - Never Gonna Give You Up";
+document.body.firstElementChild.innerHTML = "Rick Astley - Never Gonna Give You Up";
+const couplets = document.getElementsByClassName("couplet");
+couplets[0].innerHTML = 
