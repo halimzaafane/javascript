2818cb3c5fc087226c5a6af824b459c4aab6d28c
halimzaafane <109077943+halimzaafane@users.noreply.github.com>
2022-07-11 21:13:16 +0200

    update11/07/22

--- codinggame/exo1/exo1.js
+++ codinggame/exo1/exo1.js
@@ -1,4 +1,14 @@
 //document.getElementById('titre').textContent = "Rick Astley - Never Gonna Give You Up";
 document.body.firstElementChild.innerHTML = "Rick Astley - Never Gonna Give You Up";
 const couplets = document.getElementsByClassName("couplet");
-couplets[0].innerHTML = 
+
+for (let i=0; i < couplets.length; i++) {
+
+
+    // faire le if ...
+
+    // suppression du texte
+    couplets[i].removeChild(couplets[i].firstChild);
+    // supression de la balise br
+    couplets[i].removeChild(couplets[i].firstElementChild);
+}
