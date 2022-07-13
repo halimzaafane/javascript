f448a8465d265d54f366b7f5df877301fd5413b4
halimzaafane <109077943+halimzaafane@users.noreply.github.com>
2022-07-13 14:57:59 +0200

    update13/07/22

--- codinggame/exo3/index.js
+++ codinggame/exo3/index.js
@@ -1,6 +1,7 @@
   $(document).ready(function(){
     $("#masquer-paroles").click(function(){
       $("p").toggle();
+      
       $(document).ready(function(){
         $("#masquer_paroles").val("Afficher les paroles");
       });
