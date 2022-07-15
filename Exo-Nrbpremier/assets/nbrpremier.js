1f21ecd2e5d761a61aa3ee119532624c4a2b13fb
halimzaafane <109077943+halimzaafane@users.noreply.github.com>
2022-07-15 07:43:15 +0200

    update15/07/22

--- Exo-Nrbpremier/assets/nbrpremier.js
+++ Exo-Nrbpremier/assets/nbrpremier.js
@@ -1,15 +1,20 @@
-var n = new Number;
-estPrem = new Boolean(true);
-function tester()
-{
-  n=Number(document.test.nbr.value);
-  estPrem=true;
-  for ( i = 2; i < n; i++)
-  {
-    if (n%i==0)estPrem=false;
-    let html = document.getElementById("reponse").innerHTML=estPrem;
-  }
+var input = new Number;
+// estPrem = new Boolean(true);
+function testerNbr() {
+    input = Number(document.form.user.value);
+    estPrem = true;
 
-    document.test.rep.value = estPrem;
+    for (i = 2; i < input; i++) {
+        if (input % i == 0) estPrem = false
+        }
+        
 
-  }
+    document.form.result.value = estPrem;
+    if(estPrem === true) {
+        document.getElementById("reponse").innerHTML = `Le nombre ${input} est un nombre Premier`;
+    } else {
+        document.getElementById("reponse").innerHTML = `Le nombre ${input} n'est pas un nombre Premier`;
+    }
+
+
+}  
